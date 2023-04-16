ebe09d8231fea30b276672cf9cd3fca59ce8b9e2
DadaLV <vynohradova.l.a@gmail.com>
2023-04-16 22:19:12 +0200

    modify stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: 'pk_f99099d9f99c4027bd994c4a97fd753a',
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+    client.price(ticker_symbol)
+  end
 end
